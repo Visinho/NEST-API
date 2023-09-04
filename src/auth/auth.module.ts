46b0bad6964e08f65d5dfa5a33995e66514a5adf
Visinho <Elvisvisinho@rocketmail.com>
2023-09-04 07:27:13 +0100

    Generated access token

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,11 @@ import { PrismaModule } from './../prisma/prisma.module';
 import { Module } from "@nestjs/common";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
+import { JwtModule } from '@nestjs/jwt';
 
 
 @Module({
+    imports: [JwtModule.register({})],
     controllers: [AuthController],
     providers: [AuthService]
 })
